f2eb3e44940cdd113a7d4de0e28923be3290972d
celilkirca <tp.celil.kirca@vakifkatilim.com.tr>
2022-12-23 01:30:39 +0300

    initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticlePreviewFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticlePreviewFragment : Fragment(R.layout.item_article_preview) {
+}
